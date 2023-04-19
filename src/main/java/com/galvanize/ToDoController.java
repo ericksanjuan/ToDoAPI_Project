6bef5049327556a62c8779cad6c398d4cc6557ba
Erick San Juan <esanjuanalvarez@outlook.com>
2023-04-19 15:35:56 -0500

    Red: getAll_UnDone_Todos: did not set up a get mapping

---
+++ src/main/java/com/galvanize/ToDoController.java
@@ -0,0 +1,18 @@
+package com.galvanize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/todo")
+public class ToDoController {
+        List<ToDo> toDoList = new ArrayList<>();
+        public ToDoController() {
+            toDoList.add(new ToDo(1,"Clean my room"));
+            toDoList.add(new ToDo(2,"Dishes"));
+            toDoList.add(new ToDo(3,"Walk the Dog"));
+        }
+}
---
+++ src/test/java/com/galvanize/ToDoControllerTests.java
@@ -0,0 +1,44 @@
+package com.galvanize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import org.springframework.http.MediaType;
+
+@WebMvcTest
+public class ToDoControllerTests {
+    @Autowired
+    private MockMvc toDo;
+
+//    {
+//  "id": 1,
+//  "description": "Clean my room",
+//  "complete": false
+//}
+//    @Test
+//    public void getTodoList_noParams() throws Exception {
+//            toDo.perform(get("/todo"))
+//                    .andDo(print())
+//                    .andExpect(status().isOk())
+//                    .andExpect(jsonPath("$.id").value(1))
+//                    .andExpect(jsonPath("$.description").value("Clean my Room"))
+//                    .andExpect(jsonPath("$.complete").value(false));
+//    }
+
+    @Test
+    public void getAll_UnDone_Todos() throws Exception {
+        toDo.perform(get("/todo"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Clean my room"));
+    }
+}
